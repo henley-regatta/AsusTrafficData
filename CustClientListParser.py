3702a9cafc183025566b282b4bd1e695a81c1fd8
henley-regatta <42374576+henley-regatta@users.noreply.github.com>
2021-06-11 12:18:02 +0100

    Parser script for ASUS custom_clientlist file

---
+++ CustClientListParser.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+##############################################################################
+# CustClientListParser.py
+# -----------------------
+#
+# ASUS Routers - at least those running asuswrt-merlin firmware - store the
+# mappings a user makes to the client list in a file nominally stored in
+#   /jffs/nvram/custom_clientlist.
+# This (essentially) maps MAC addresses to "friendy names". There's a couple
+# of other integer fields too but I can't find documentation on what they're for.
+#
+# The format of this file is a *weird* angle-bracket delimiter. This script
+# (Class) provides a way to turn this into standard Python Dict objects, by
+# default using MAC address as the key.
+###############################################################################
+
+
+class CustClientListParser(object):
+
+    def __init__(self, rawclientdata):
+        self.mactoname = {}
+        self.rawdata = rawclientdata
+        self._parseclientdata()
+
+    def _parseclientdata(self):
+        #Internal format of the clientlist is similar to a repetition of:
+        # <Name>MacAddress>Int1>Int2>>
+        #(noting that "<" and ">" are literals not representative)
+        kvp = self.rawdata.split(">>") # Split on "record delimiter"
+        if len(kvp)<2 :
+            raise TypeError("Passed incorrect ASUS custom_clientlist data")
+        for rec in kvp:
+            if len(rec)==0 :
+                continue #Ignore blank records
+            fields = rec.split(">")
+            if len(fields) < 2 :
+                raise TypeError(f"Invalid record in custom_clientlist data: {rec}")
+            if fields[0][:1] != "<" :
+                raise ValueError(f"Invalid NAME field in record {rec} ({fields[0]})")
+            if len(fields[1]) != 17 :
+                raise ValueError(f"Invalid MAC field in record {rec} ({fields[1]})")
+
+            self.mactoname[fields[1]] = fields[0][1:] #Strip the "<" before appending
+
+    def getMappings(self):
+        return self.mactoname
+
+if __name__ == "__main__":
+    print(f"This is a TEST PROGRAM")
+    from os.path import isfile
+    from sys import argv
+    if len(argv) == 2 and isfile(argv[1]):
+        with open(argv[1]) as ccf:
+            namemappings = CustClientListParser(ccf.read()).getMappings()
+            for mac in namemappings:
+                print(f"{mac} -> {namemappings[mac]}")
+
+    else :
+        print("When called directly, supply the path to an ASUS Router custom_clientlist file on the command line")
